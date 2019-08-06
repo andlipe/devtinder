f3901dc0334aed7875dc384169854cd54e98049f
André Moura <andrefelipebarbosamoura@gmail.com>
2019-08-06 16:35:31 -0300

    todos os arquivos

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://omni:omnistack@devtinder-dvzwi.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(express.json);
+server.use(routes);
+
+server.listen(3000);
